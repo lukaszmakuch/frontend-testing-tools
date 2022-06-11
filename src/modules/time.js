d60f2ae649fa549e4ef2138d76e7f0e3e1e7b9ef
Łukasz Makuch <zopsesen@gmail.com>
2022-06-11 17:48:55 +0200

    time utils

--- src/modules/time.js
+++ src/modules/time.js
@@ -1,6 +1,11 @@
 module.exports = {
   mock: async function (ISO) {
     const timestamp = +new Date(ISO);
-    await this.driver.executeScript(`mockTime(arguments[0])`, timestamp);
+    await this.driver.executeScript(
+      `
+        Date.now = () => arguments[0]
+      `,
+      timestamp
+    );
   },
 };
